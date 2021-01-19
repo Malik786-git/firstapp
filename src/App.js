2c19261a8cf99359c76b38a9705a1bf85b36fad3
Malik M Jahangir <73543873+Malik786-git@users.noreply.github.com>
2021-01-20 00:33:45 +0500

    Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       <h1>My First React App</h1>
+       <h1>My First React App Successfully Deploye on Surge</h1>
     </div>
   );
 }
